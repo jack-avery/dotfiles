8cac1488a76ebd5fbf558e508a187a95ab262e1c
jack-avery <email.jack.avery@gmail.com>
2023-12-14 12:07:34 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
+# create directory structure
 DIRS=( "$HOME/.oh-my-zsh/custom/themes" "$HOME/.config/nvim" "$HOME/.config/neofetch" )
-for dir in ${DIRS[@]} do
+for dir in ${DIRS[@]}; do
     [[ ! -e $dir ]] && mkdir -pv $dir
 done
 
+# delete any pre-existing (default?) config files
+OLD=( "$HOME/.oh-my-zsh/custom/themes/jack.zsh-theme" "$HOME/.config/nvim/init.vim" "$HOME/.config/neofetch/config.conf" "$HOME/.zshrc" )
+for file in ${OLD[@]}; do
+    [[ -f $file ]] && mv -v $file $file.old
+    [[ -L $file ]] && rm -v $file
+done
+
+# link my config
 ln -sv $(pwd)/jack.zsh-theme ~/.oh-my-zsh/custom/themes/jack.zsh-theme
 ln -sv $(pwd)/neovim ~/.config/nvim/init.vim
 ln -sv $(pwd)/neofetch ~/.config/neofetch/config.conf
