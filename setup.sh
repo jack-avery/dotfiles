6807e555bf1ab2d3a89561eaa6aad52666424864
jack-avery <email.jack.avery@gmail.com>
2023-12-15 00:10:04 -0500

    attempt fix

--- setup.sh
+++ setup.sh
@@ -8,10 +8,10 @@ DOTS=(  ["zsh_theme"]="$HOME/.oh-my-zsh/custom/themes/jack.zsh-theme" \
 
 for d in ${!DOTS[@]}; do
     # create folders if they don't exist
-    [[ ! -e ${DOTS[$d]} ]] && mkdir -pv 
+    [[ ! -e $(dirname "${DOTS[$d]}") ]] && mkdir -pv $(dirname ${DOTS[$d]})
     # delete old files/residual symlinks
-    [[ -f ${DOTS[$d]} ]] && mv -v $d $d.old
-    [[ -L ${DOTS[$d]} ]] && rm -v $d
+    [[ -f ${DOTS[$d]} ]] && mv -v ${DOTS[$d]} ${DOTS[$d]}.old
+    [[ -L ${DOTS[$d]} ]] && rm -v ${DOTS[$d]}
     # symlink new config
     ln -sv $(pwd)/$d ${DOTS[$d]}
 done
