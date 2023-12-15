815009f0f58371b4a8f0d52a7d7ec7519d4a70b4
jack-avery <email.jack.avery@gmail.com>
2023-12-15 00:08:31 -0500

    conjoin

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
-# create directory structure
-DIRS=( "$HOME/.oh-my-zsh/custom/themes" "$HOME/.config/nvim" "$HOME/.config/neofetch" )
-for dir in ${DIRS[@]}; do
-    [[ ! -e $dir ]] && mkdir -pv $dir
-done
+#!/usr/bin/env bash
 
 declare -A DOTS
 DOTS=(  ["zsh_theme"]="$HOME/.oh-my-zsh/custom/themes/jack.zsh-theme" \
         ["nvim"]="$HOME/.config/nvim/init.vim" \
         ["neofetch"]="$HOME/.config/neofetch/config.conf" \
         ["zshrc"]="$HOME/.zshrc" )
+
 for d in ${!DOTS[@]}; do
+    # create folders if they don't exist
+    [[ ! -e ${DOTS[$d]} ]] && mkdir -pv 
     # delete old files/residual symlinks
     [[ -f ${DOTS[$d]} ]] && mv -v $d $d.old
     [[ -L ${DOTS[$d]} ]] && rm -v $d
