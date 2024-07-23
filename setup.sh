0f532e28dbac0a4f957fde5d250b76c3f9b0c968
jack-avery <47289484+jack-avery@users.noreply.github.com>
2024-07-23 10:40:15 -0400

    introduce i3, scripts

--- setup.sh
+++ setup.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+DIR=$(dirname "$0")
+if [[ $DIR == '.' ]] ; then
+    DIR=$(pwd)
+fi
+
+VERBOSE=''
+if [[ $1 == '-v' ]] ; then
+    VERBOSE='-v'
+fi
+
 declare -A DOTS
-DOTS=(  ["aliases"]="$HOME/.aliases" \
+DOTS=(  ["shellrc"]="$HOME/.shellrc" \
+        ["aliases"]="$HOME/.aliases" \
         ["nvim"]="$HOME/.config/nvim" \
         ["vimrc"]="$HOME/.vimrc" \
         ["bashrc"]="$HOME/.bashrc" \
@@ -9,15 +20,27 @@ DOTS=(  ["aliases"]="$HOME/.aliases" \
         ["zsh_theme"]="$HOME/.oh-my-zsh/custom/themes/jack.zsh-theme" \
         ["gitconfig"]="$HOME/.gitconfig" \
         ["tmux"]="$HOME/.config/tmux/tmux.conf" \
-        ["alacritty"]="$HOME/.config/alacritty.toml" )
+        ["alacritty"]="$HOME/.config/alacritty.toml" \
+        ["i3"]="$HOME/.config/i3/config" )
 
 for d in ${!DOTS[@]}; do
     # create folders if they don't exist
-    [[ ! -e $(dirname "${DOTS[$d]}") ]] && mkdir -pv $(dirname ${DOTS[$d]})
+    [[ ! -e $(dirname "${DOTS[$d]}") ]] && mkdir -p $VERBOSE $(dirname ${DOTS[$d]})
     # delete old files/residual symlinks
-    [[ -L ${DOTS[$d]} ]] && rm -v ${DOTS[$d]}
-    [[ -f ${DOTS[$d]} ]] && mv -v ${DOTS[$d]} ${DOTS[$d]}.old
+    [[ -L ${DOTS[$d]} ]] && rm $VERBOSE ${DOTS[$d]}
+    [[ -f ${DOTS[$d]} ]] && mv $VERBOSE ${DOTS[$d]} ${DOTS[$d]}.old
     # symlink new config
-    ln -sv $(pwd)/$d ${DOTS[$d]}
+    ln -s $VERBOSE $DIR/$d ${DOTS[$d]}
 done
+
+[[ ! -e "$HOME/.local/bin" ]] && mkdir -p $VERBOSE "$HOME/.local/bin"
+for SCRIPT in $DIR/scripts/*; do
+    BASENAME=$(basename "$SCRIPT")
+    chmod $VERBOSE +x $DIR/scripts/$BASENAME
+    # replace with script
+    [[ -L $HOME/.local/bin/$BASENAME ]] && rm $VERBOSE $HOME/.local/bin/$BASENAME
+    ln -s $VERBOSE $DIR/scripts/$BASENAME $HOME/.local/bin/$BASENAME
+done
+
 echo finished
+
