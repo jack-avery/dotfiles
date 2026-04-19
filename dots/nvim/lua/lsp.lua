5563d7fe4890d57c96ee4e62a6523a34ffb11f29
jack-avery <47289484+jack-avery@users.noreply.github.com>
2026-04-18 23:41:00 -0400

    update

--- dots/nvim/lua/lsp.lua
+++ dots/nvim/lua/lsp.lua
@@ -70,3 +70,6 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('lspconfig')["pylsp"].setup {
   capabilities = capabilities
 }
+require('lspconfig')["clangd"].setup {
+  capabilities = capabilities
+}
